06694253dcd406294773d39b57116568f488499a
FruityLoopes <chadandrews07@gmail.com>
2023-08-28 17:01:43 +0200

    RecyclerView works

--- app/src/main/java/com/example/prjopsc7312ivetask3/LocationAdapter.kt
+++ app/src/main/java/com/example/prjopsc7312ivetask3/LocationAdapter.kt
@@ -1,4 +1,42 @@
 package com.example.prjopsc7312ivetask3
 
-class LocationAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+
+class LocationAdapter: ListAdapter<Location, LocationAdapter.LocationAdapter>(LocationViewHolder())
+{
+    class LocationAdapter(view : View): RecyclerView.ViewHolder(view) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LocationAdapter {
+        val inflater = LayoutInflater.from(parent.context)
+        return com.example.prjopsc7312ivetask3.LocationAdapter.LocationAdapter(inflater.inflate(
+            R.layout.view_layout,parent,false))
+    }
+
+    override fun onBindViewHolder(holder: LocationAdapter, position: Int) {
+        val local = getItem(position)
+        holder.itemView.findViewById<TextView>(R.id.txtLat).text = "Lat: " + local.Lat
+        holder.itemView.findViewById<TextView>(R.id.txtLong).text = "Long: " +local.Long
+        holder.itemView.findViewById<TextView>(R.id.txtTime).text = "TimeStamp: " +local.TimeStamp
+        holder.itemView.findViewById<TextView>(R.id.txtAddress).text = "Address: " +local.Address
+    }
+
+}
+
+class LocationViewHolder : DiffUtil.ItemCallback<Location>() {
+    override fun areItemsTheSame(oldItem: Location, newItem: Location): Boolean {
+        return oldItem.Address == newItem.Address
+    }
+
+    override fun areContentsTheSame(oldItem: Location, newItem: Location): Boolean {
+        return areItemsTheSame(oldItem,newItem)
+    }
+
 }
--- app/src/main/java/com/example/prjopsc7312ivetask3/MainActivity.kt
+++ app/src/main/java/com/example/prjopsc7312ivetask3/MainActivity.kt
@@ -7,7 +7,6 @@ import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import android.Manifest
 import android.content.ContentValues.TAG
@@ -16,18 +15,30 @@ import android.location.Geocoder
 import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import java.util.Locale
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.FormatStyle
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity(), LocationListener {
 
     private lateinit var locationManager : LocationManager
     private val locationPermissionCode  = 2
+    private  lateinit var locationAdapter: LocationAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val button : Button = findViewById(R.id.btnLocation)
+
+        locationAdapter = LocationAdapter()
 
         button.setOnClickListener(){
             getLocation()
@@ -47,13 +58,28 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
     override fun onLocationChanged(location: Location) {
 
-
+        val view: RecyclerView = findViewById(R.id.recyclerView)
         val geocoder = Geocoder(this , Locale.getDefault())
         val addresses = geocoder.getFromLocation(location.latitude , location.longitude, 1)
+        val current = LocalDateTime.now()
+        val formatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
+        val formatted = current.format(formatter)
 
+        val gsonPretty = GsonBuilder().setPrettyPrinting().create()
 
-        Log.d(TAG, addresses?.get(0)?.getAddressLine(0).toString())
-        Log.d(TAG, location.latitude.toString() + " " + location.latitude.toString())
+        val details = Location(location.latitude.toString() , location.longitude.toString() ,  formatted.toString() , addresses?.get(0)?.getAddressLine(0).toString() )
+        val json = gsonPretty.toJson(details)
+        val detailsList = Gson().fromJson( "["+ json + "]" , Array<com.example.prjopsc7312ivetask3.Location>::class.java).toList()
+
+        view.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = locationAdapter
+        }
+
+        locationAdapter.submitList(detailsList)
+
+        Log.d(TAG, json)
+
 
     }
     override fun onRequestPermissionsResult(
